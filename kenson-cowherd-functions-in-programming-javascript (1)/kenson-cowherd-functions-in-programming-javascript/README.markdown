946a000abfe7c29592c0d0379343637b0d0228bd
kenn0611 <83709575+kenn0611@users.noreply.github.com>
2021-05-29 21:51:53 -0400

    Add files via upload

---
+++ kenson-cowherd-functions-in-programming-javascript (1)/kenson-cowherd-functions-in-programming-javascript/README.markdown
@@ -0,0 +1,5 @@
+# Kenson Cowherd Functions in Programming JavaScript
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Kenn0611/pen/rNyeVrg](https://codepen.io/Kenn0611/pen/rNyeVrg).
+
+
---
+++ kenson-cowherd-functions-in-programming-javascript (1)/kenson-cowherd-functions-in-programming-javascript/src/script.js
@@ -0,0 +1,43 @@
+var number = 0;
+
+function writeNumber(){
+  document.querySelector("#numbers").innerHTML=number;
+}
+ 
+function addMe(){
+  number = number+1;
+  
+  writeNumber();
+}
+
+writeNumber();
+
+
+var number = 0;
+
+function writeNumber(){
+  document.querySelector("#numbers").innerHTML=number;
+}
+
+function addMe(){
+  number = number+1;
+  
+  writeNumber();
+}
+
+writeNumber();
+
+
+var number = 0;
+
+function writeNumber(){
+  document.querySelector("#numbers").innerHTML=number;
+}
+
+function addMe(){
+  number = number+1;
+  
+  writeNumber();
+}
+
+writeNumber();
